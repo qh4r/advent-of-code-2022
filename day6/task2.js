f56eff085864a5643a80ba6bd916bad33736d9a5
Rafał Kucharski <rafal.kucharski@thesoftwarehouse.pl>
2022-12-06 08:04:17 +0100

    Day 6

---
+++ day6/task2.js
@@ -0,0 +1,8 @@
+const data = require('fs').readFileSync('data6.txt').toString()
+const searchArr = [];
+const res = data.split('').findIndex((letter, i) => {
+    searchArr.push(letter);
+    if(searchArr.length > 14) searchArr.shift()
+    return Array.from(new Set(searchArr)).length === 14
+})
+console.log('task1 ->', res + 1);
